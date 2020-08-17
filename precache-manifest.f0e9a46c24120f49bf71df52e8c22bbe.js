ea5cac51603461d7f7096ed5387e339070876dda
Farid Hernandez <farid.hdz.64@gmail.com>
2020-08-16 19:43:10 -0600

    Updates

---
+++ precache-manifest.f0e9a46c24120f49bf71df52e8c22bbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b6c1128c4ef7a89f32c6438a00c0ea0",
+    "url": "/react-itca-webinar-full-example-2/index.html"
+  },
+  {
+    "revision": "1464dc4afc13f5744e3e",
+    "url": "/react-itca-webinar-full-example-2/static/css/main.838a75c2.chunk.css"
+  },
+  {
+    "revision": "3acac6fc9f311e537884",
+    "url": "/react-itca-webinar-full-example-2/static/js/2.48aef5f6.chunk.js"
+  },
+  {
+    "revision": "556d72da1c19c1e88e2917fa6505d7f0",
+    "url": "/react-itca-webinar-full-example-2/static/js/2.48aef5f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1464dc4afc13f5744e3e",
+    "url": "/react-itca-webinar-full-example-2/static/js/main.c0614e93.chunk.js"
+  },
+  {
+    "revision": "e312f4399d97028c1b34",
+    "url": "/react-itca-webinar-full-example-2/static/js/runtime-main.2374cdf8.js"
+  }
+]);
